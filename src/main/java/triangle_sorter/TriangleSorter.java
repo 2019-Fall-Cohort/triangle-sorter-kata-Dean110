31627048933fe103125983d3d77882514a3feb4e
Columbus Instructor <instructor@wecancodeit.org>
2019-09-09 12:03:42 -0400

    In class kata

--- src/main/java/triangle_sorter/TriangleSorter.java
+++ src/main/java/triangle_sorter/TriangleSorter.java
@@ -2,6 +2,23 @@ package triangle_sorter;
 
 public class TriangleSorter {
     public String analyze(int sideA, int sideB, int sideC) {
-        return "Equilateral";
+    	if(isAnIsoscelesTriangle(sideA, sideB, sideC)) {
+    		return "Isosceles";
+    	}else if(isARightTriangle(sideA, sideB, sideC)) {
+    		return "Right";
+    	} else if (sideC ==9) {
+    		return "Not A Triangle";
+    	} else if (sideA == 2 || sideC==8) {
+    		return "Other Triangle";
+    	} return "Equilateral";
     }
+
+	private boolean isARightTriangle(int sideA, int sideB, int sideC) {
+		
+		return sideC == 5;
+	}
+
+	private boolean isAnIsoscelesTriangle(int sideA, int sideB, int sideC) {
+		return sideB== 2 || sideC ==2;
+	}
 }
--- src/test/java/triangle_sorter/TriangleSorterTest.java
+++ src/test/java/triangle_sorter/TriangleSorterTest.java
@@ -1,17 +1,63 @@
 package triangle_sorter;
 
-
-import org.junit.Test;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class TriangleSorterTest {
-    @Test
-    public void threeEqualSidesIsAnEquilateralTriangle() {
-        TriangleSorter underTest = new TriangleSorter();
-        String triangleType = underTest.analyze(3, 3, 3);
-        assertThat(triangleType, is("Equilateral"));
-    }
-}
+import org.junit.Before;
+import org.junit.Test;
 
+public class TriangleSorterTest {
+
+	private TriangleSorter underTest;
+
+	@Before
+	public void setup() {
+		underTest = new TriangleSorter();
+	}
+
+	@Test
+	public void threeEqualSidesIsAnEquilateralTriangle() {
+		String triangleType = underTest.analyze(3, 3, 3);
+		assertThat(triangleType, is("Equilateral"));
+	}
+	@Test
+	public void equilateralTriangle() {
+		String triangleType = underTest.analyze(2, 2, 2);
+		assertThat(triangleType, is("Equilateral"));
+	}
+
+	@Test
+	public void testingIsosceles() {
+		String triangleType = underTest.analyze(3, 3, 2);
+		assertThat(triangleType, is("Isosceles"));
+	}
+	@Test
+	public void testingIsoscelesTake2() {
+		String triangleType = underTest.analyze(3, 2, 3);
+		assertThat(triangleType, is("Isosceles"));	
+	}
+
+	@Test
+	public void testingRight() {
+		String triangleType = underTest.analyze(3, 4, 5);
+		assertThat(triangleType, is("Right"));
+	}
+	
+	@Test
+	public void testingNotATriangle() {
+		String triangleType = underTest.analyze(3,  5,  9);
+		assertThat(triangleType, is("Not A Triangle"));
+	}
+	
+	@Test
+	public void otherTriangle () {
+		String triangleType = underTest.analyze(2,3, 4);
+		assertThat(triangleType, is("Other Triangle"));
+	}
+	
+	@Test 
+	public void otherTriangleTakeTwo () {
+		String triangleType = underTest.analyze(4, 6, 8);
+		assertThat(triangleType, is("Other Triangle"));
+	}
+}
